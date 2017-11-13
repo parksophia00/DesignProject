405686b90c5594e0ad3c00c773fdd93635824072
ForestAPC <acbrixia@gmail.com>
2017-11-13 01:45:45 -0500

    Update FinishMonsters.java

--- FinishMonsters.java
+++ FinishMonsters.java
@@ -1,9 +1,36 @@
 package dasher;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 
-public class FinishMonsters {
+import javax.swing.JPanel;
 
-	public FinishMonsters() {
-		// TODO Auto-generated constructor stub
+public class FinishMonsters extends JPanel
+{
+	private long Time;
+	
+	public FinishMonsters(long Time)
+    {
+    	setLayout(null);
+    	setSize(800,700);
+    	setOpaque(true);
+    	setVisible(true);
+    }
+	
+	public void paint(Graphics g) 
+	{	
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 800, 700);
+		g.setColor(Color.YELLOW);
+	    g.setFont(new Font("TimesRoman", Font.PLAIN, 80)); 
+	    g.drawString("Nice! You got the monsters!", 100, 350);
+	    g.setFont(new Font("TimesRoman", Font.PLAIN, 30)); 
+	    System.out.println(Time);
+	}
+	
+	public void SetTime(long temp) 
+	{	
+		Time = temp;
 	}
 
 }
