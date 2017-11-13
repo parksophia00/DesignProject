8c30a1c2f1a04d867b4f54ca3671b77d930909f2
parksophia00 <33318763+parksophia00@users.noreply.github.com>
2017-11-13 03:30:58 -0500

    Add files via upload

--- Character.java
+++ Character.java
@@ -19,8 +19,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Character extends JLabel implements KeyListener, ActionListener {
-	
+public class Character extends JLabel implements KeyListener, ActionListener 
+{
 	private ImageIcon iconleft;
 	private ImageIcon iconup;
 	private ImageIcon iconslap1;
@@ -28,6 +28,7 @@ public class Character extends JLabel implements KeyListener, ActionListener {
 	private ImageIcon thecos;
 	int dx = 0;
 	int dy = 0;
+	//int a = 0;
 	double yaccel = 100;
 	private HP health;
 	private MP mana;
@@ -36,78 +37,70 @@ public class Character extends JLabel implements KeyListener, ActionListener {
 	boolean wait = true;
 	boolean turn = false;
 	boolean ground = true;
-	
-    public Character() 
-    {
-    	health = new HP(400);
-    	mana = new MP(300);
-    
-    	
-        iconleft = new ImageIcon("C:/Users/Celeste/eclipse-workspace/CSC 405/src/alert_0.png");
-        iconup = new ImageIcon("C:/Users/Celeste/eclipse-workspace/CSC 405/src/fly_0.png");
-        iconslap1 = new ImageIcon("C:/Users/Celeste/eclipse-workspace/CSC 405/src/stabO1_0.png");
-        iconslap2 = new ImageIcon("C:/Users/Celeste/eclipse-workspace/CSC 405/src/stabO1_0.png");
-        
-        setIcon(iconleft);
-        setBounds(350,250,200,200);
-        setVisible(true);
-        
-        System.out.println(health);
 
-    }
-     public boolean endCheck()
-    {
-    	boolean x = false;
-		if(350-dx==0) {
-    		x = true;
+	public Character() 
+	{
+		health = new HP(400);
+		mana = new MP(300);
+
+		iconleft = new ImageIcon("alert_0.png");
+		iconup = new ImageIcon("fly_0.png");
+		iconslap1 = new ImageIcon("stabO1_0.png");
+		iconslap2 = new ImageIcon("stabO1_0.png");
+
+		setIcon(iconleft);
+		setBounds(350,250,200,200);
+		setVisible(true);
+
+		System.out.println(health);
+	}
+
+	public int getHP()
+	{
+		return health.getHP();
+	}
+
+	public int getMP()
+	{
+		return mana.getMP();
+	}
+
+	public void Left()
+	{
+		if(350+dx>=0)
+		{
+			dx = dx - 50;
+			setBounds(350+dx,250-dy,200,200);
 		}
-    		return x;
-    }
-    public int getHP()
-    {
-    	return health.getHP();
-    }
-    
-    
-    public int getMP()
-    {
-    	return mana.getMP();
-    }
-    
-    public void Left()
-    {
-    	if(350+dx>=0)
-        {
-    	dx = dx - 50;
-        setBounds(350+dx,250-dy,200,200);
-        }
-    	if(350+dx<0)
-    	        try {
-    	            // Open an audio input stream.
-    	        	File soundFile = new File("C:/Users/Celeste/eclipse-workspace/CSC 405/src/wall.wav");
-    	        	AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-    	            // Get a sound clip resource.
-    	            Clip clip = AudioSystem.getClip();
-    	            // Open audio clip and load samples from the audio input stream.
-    	            clip.open(audioIn);
-    	            clip.start();
-    	         } catch (UnsupportedAudioFileException e) {
-    	            e.printStackTrace();
-    	         } catch (IOException e) {
-    	            e.printStackTrace();
-    	         } catch (LineUnavailableException e) {
-    	            e.printStackTrace();
-    	         }
-    }
 
-    public void Right()
-    {
-    	if(350+dx<=670)
-    	{
-    	dx = dx + 50;
-    	setBounds(350+dx,250-dy,200,200);
-    	}
-    	if(350+dx>670)
+		/*if (350+dx <0)
+		try {
+            // Open an audio input stream.
+        	File soundFile = new File("wall.wav");
+        	AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            // Get a sound clip resource.
+            Clip clip = AudioSystem.getClip();
+            // Open audio clip and load samples from the audio input stream.
+            clip.open(audioIn);
+            clip.start();
+         } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+         } catch (IOException e) {
+            e.printStackTrace();
+         } catch (LineUnavailableException e) {
+            e.printStackTrace();
+         }*/
+	}
+
+	public void Right()
+	{
+		if(350+dx<=670)
+		{
+			dx = dx + 50;
+			setBounds(350+dx,250-dy,200,200);
+			//	a = 350+dx;
+		}
+		/*	if(350+dx>670)
 	        try {
 	            // Open an audio input stream.
 	        	File soundFile = new File("C:/Users/Celeste/eclipse-workspace/CSC 405/src/wall.wav");
@@ -123,16 +116,36 @@ public class Character extends JLabel implements KeyListener, ActionListener {
 	            e.printStackTrace();
 	         } catch (LineUnavailableException e) {
 	            e.printStackTrace();
-	         }
-    }
-    
+	         }*/
+	}
 
-    public void Up()
-    {
-    	ground = false;
-    	if (wait)															//prevent double jumps -- check platform
-    	{
-            try {
+	/*public int locationX()
+	{
+		return (a+400);
+	}
+	//public int locationY()
+	{
+		//return 
+	}*/
+	
+	/*public boolean ground()
+	{
+		return 
+	}*/
+	public void Up()
+	{
+		ground = false;
+		if (wait)															//prevent double jumps -- check platform
+		{
+			yaccel = .005;
+			setIcon(iconup);
+
+			t = new Timer(10,this);
+			t.start();   
+
+			time=50;
+
+			/* try {
                 // Open an audio input stream.
             	File soundFile = new File("C:/Users/Celeste/eclipse-workspace/CSC 405/src/jump.wav");
             	AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
@@ -147,145 +160,143 @@ public class Character extends JLabel implements KeyListener, ActionListener {
                 a.printStackTrace();
              } catch (LineUnavailableException a) {
                 a.printStackTrace();
-             }
-    	yaccel = .005;
-        setIcon(iconup);
-        
-        t = new Timer(10,this);
-        t.start();   
-        
-        time=50;
-    	}
-        
-    }  
-    
-        @Override
-            public void actionPerformed(ActionEvent e)
-        	{
-        		wait=false;
-        		if(!turn)
-        		{
-        		time--;
-            	dy =  (int) (dy + 0.5*yaccel*java.lang.Math.pow(time,2));
-            	setBounds(350+dx,250-dy,200,200);
-            	System.out.println(time);
-            	System.out.println("BOOGERS" + (250-dy));
-        		}
-            	if(time==0)
-            	{
-            		t.stop();
-            		turn=!turn;
-            	}
-            	if(turn)
-        		{
-            		t.start();
-	        		time++;
-	            	dy =  (int) (dy + 0.5*-yaccel*java.lang.Math.pow(time,2));
-	            	setBounds(350+dx,250-dy,200,200);
-	            	System.out.println(time);
-        		}
-            	if(ground)														//change to when touch platform
-            	{
-            		t.stop();
-            		turn=!turn;
-            		wait=true;
-                    setIcon(iconleft);
-            	}    	
-            }   
-        
-    public void Recover()
-    {
-    	dx = 0;
-    	dy = 0;
-    	setBounds(350+dx,250+dy,200,200);
-    }
-    
-    public void Down()
-    {
-    	//dy = dy + 10;
-    	//setBounds(350+dx,250+dy,200,200);
-    }
-
-    public void Floor()
-    {
-    	ground=true;
-    }
-       
-	public void Fall()
-    {
-    	//dy = dy + 50;
-    	//setBounds(350+dx,250+dy,200,200);
-    	setIcon(iconleft);
+             }*/
+		} 
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		wait=false;
+		if(!turn)
+		{
+			time--;
+			dy =  (int) (dy + 0.5*yaccel*java.lang.Math.pow(time,2));
+			setBounds(350+dx,250-dy,200,200);
+			System.out.println(time);
+			System.out.println("BOOGERS" + (250-dy));
+		}
+		if(time==0)
+		{
+			t.stop();
+			turn=!turn;
+		}
+		if(turn)
+		{
+			t.start();
+
+			time++;
+			dy =  (int) (dy + 0.5*-yaccel*java.lang.Math.pow(time,2));
+			setBounds(350+dx,250-dy,200,200);
+			System.out.println(time);
+			if (dy<0)
+			{
+				ground = true;
+			}
+		}
+		
+		if(ground)														//change to when touch platform
+		{
+			t.stop();
+			turn=!turn;
+			wait=true;
+			setIcon(iconleft);
+		}    	
+	}   
+
+	public void Recover()
+	{
+		dx = 0;
+		dy = 0;
+		setBounds(350+dx,250+dy,200,200);
+	}
+
+	public void Down()
+	{
+		//dy = dy + 10;
+		//setBounds(350+dx,250+dy,200,200);
+	}
+
+	public void Floor()
+	{
+		ground=true;
+	}
+
+	public void Fall()
+	{
+		//dy = dy + 50;
+		//setBounds(350+dx,250+dy,200,200);
+		setIcon(iconleft);
+	}
+
 	public void Hit()
 	{
-	if(getMP()-30>=0)
+		if(getMP()-30>=0)
 		{
-        try {
-            // Open an audio input stream.
-        	File soundFile = new File("C:/Users/Celeste/eclipse-workspace/CSC 405/src/pow.wav");
-        	AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-            // Get a sound clip resource.
-            Clip clip = AudioSystem.getClip();
-            // Open audio clip and load samples from the audio input stream.
-            clip.open(audioIn);
-            clip.start();
-         } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-         } catch (IOException e) {
-            e.printStackTrace();
-         } catch (LineUnavailableException e) {
-            e.printStackTrace();
-         }
-		setIcon(iconslap2);
-		mana.MPMinus(30);
+			/*try {
+					// Open an audio input stream.
+					File soundFile = new File("pow.wav");
+					AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+					// Get a sound clip resource.
+					Clip clip = AudioSystem.getClip();
+					// Open audio clip and load samples from the audio input stream.
+					clip.open(audioIn);
+					clip.start();
+				} catch (UnsupportedAudioFileException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (LineUnavailableException e) {
+					e.printStackTrace();
+				}
+			}*/
+			setIcon(iconslap2);
+			mana.MPMinus(30);
 		}
 	}
-	
-    public void BounceBack()
-    {
-        setIcon(iconleft);
-        health.HPMinus(30);
-    }
+
+	public void BounceBack()
+	{
+		setIcon(iconleft);
+		health.HPMinus(30);
+	}
 
 	public void Idle()
 	{
-	setIcon(iconleft);
+		setIcon(iconleft);
 	}
-	
+
 	public boolean MiddleCheck()
 	{
-	return(350+dx==350);
+		return(350+dx==350);
+	}
+	
+	public boolean endCheck()
+	{
+		return(dx-350==0); 
 	}
 
 	//when fall off
-    public void Die()
-    {
-        health.setHP(0);
-    }
-    
+	public void Die()
+	{
+		health.setHP(0);
+	}
+
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
 }
-
-
-
-
-
